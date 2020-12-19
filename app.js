84a3820ce72e7cfe4833f32ad2bc3845e8aaebf2
Vaibhav <Vaibhav97singh21@gmail.com>
2020-12-19 17:38:30 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-
+var txtInput = document.querySelector("#txt-input"); // selector using id of tags
+var outputDiv = document.querySelector("#output");
 
 
 btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log(txtInput.value);
+    outputDiv.innerText =  txtInput.value;
     
 })
