e425d976f4deda1ed557c63fa4a720b6a904fd68
Vaibhav <Vaibhav97singh21@gmail.com>
2020-12-19 21:31:31 +0530

    final commit

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input"); // selector using id of tags
+var btnTranslate = document.querySelector(".btn-translate");
+var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+ // api for minion translator
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+//error handling
+function errHandler(error) {
+    console.log("error occured, "+ error);
+    alert("Server is down !! try again after some time");
 
+}
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
 
 btnTranslate.addEventListener("click", function clickEventHandler(){
-    outputDiv.innerText =  txtInput.value;
-    
-})
+    var text = txtInput.value;  // taking input
+
+    fetch(getTranslationURL(text))
+    .then(response => response.json())
+    .then(json =>{ 
+        outputDiv.innerText = (json.contents.translated)})
+    .catch(errHandler);
+});
